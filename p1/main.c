24abefa2cfed02faad53c3aafbacd93c3a30f657
2022127003 <koj010919@gmail.com>
2026-05-04 15:32:18 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -9,17 +9,17 @@ int main(void){
     for (a = 1; a * 900 <= N; a++) {
         for (b = 2; a * 900 + b * 750 <= N; b += 2) {
             
-            int  = N - (a * 900 + b * 750);
+            int change= N - (a * 900 + b * 750);
 
-            if (change <= 0) {
+            if (change<= 0) {
                 continue;
             }
 
-            if (change % 200 != 0) {
+            if (change% 200 != 0) {
                 continue;
             }
 
-            c = change / 200;
+            c = change/ 200;
 
             if (c >= 1 && (c < a || c < b)) {
                 printf("%d %d %d\n", a, b, c);
